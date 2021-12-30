382ed1d254fafb2c906e25eae5c45e7e96b01427
Caleb Denio <cjdenio44@gmail.com>
2021-12-29 21:17:02 -0500

    cool stuff

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.html",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
