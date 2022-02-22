d6afa3a5e97fc10c32a3ab428b9d29f503f28de0
Caleb Denio <cjdenio44@gmail.com>
2022-02-21 20:55:38 -0500

    Move site to `source/`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  content: [
-    "./_includes/**/*.html",
-    "./_layouts/**/*.html",
-    "./_posts/*.md",
-    "./*.html",
-  ],
+  content: ["./source/**/*.{html,md}"],
   theme: {
     extend: {
       fontFamily: {
