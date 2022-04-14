6ef7cde25efaa895dfcb68408e9b7aaf7c945774
Caleb Denio <cjdenio44@gmail.com>
2022-04-14 15:25:43 -0400

    Revert "Revert "Hide scrollbar on match list""
    This reverts commit 8c83b02bd33a16a6e0fdd519e3c9761e4f935cc1.

--- source/assets/src/in-competition/components/MatchList.js
+++ source/assets/src/in-competition/components/MatchList.js
@@ -76,7 +76,7 @@ export default function MatchList({ matches }) {
   }
 
   return (
-    <div className="mt-7 flex space-x-3 overflow-auto">
+    <div className="mt-7 flex space-x-3 overflow-auto hide-scrollbar">
       {playedMatches.length > 0 && (
         <MatchListSection title="Recently Played" matches={playedMatches} />
       )}
